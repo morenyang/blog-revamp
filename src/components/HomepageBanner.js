20063ec450225108e2f942970f0e453de8961a8c
Teng YANG <morenyang88@gmail.com>
2019-09-09 22:27:52 +0800

    Add gallery

--- src/components/HomepageBanner.js
+++ src/components/HomepageBanner.js
@@ -1,30 +1,48 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import styles from './HomepageBanner.module.scss'
+import HomePageBannerGallery from './HomepageBannerGallery'
+import { ParentSize } from '@vx/responsive'
 
 const HomepageBanner = () => {
+  const bannerText = (
+    <>
+      <strong>Cool things</strong> <br />
+      inside.
+    </>
+  )
+
   let bannerTextShadow = []
   for (let i = 0; i < 1000; i++) {
     bannerTextShadow.push(`${i}px ${i}px #e8e8e8`)
   }
 
   return (
-    <div className={styles.homepageBanner}>
-      <div className={styles.homepageBannerInner}>
-        <div
-          className={styles.banner}
-          style={{ textShadow: bannerTextShadow.join(', ') }}
-        >
-          <strong>Cool things</strong> <br />
-          inside.
-        </div>
-        <div className={styles.bannerFeature}>
-          <Link to={'/articles'}>
-            <button className={styles.featureBtn}>View Articles&nbsp; ›</button>
-          </Link>
+    <>
+      <div className={styles.homepageBanner}>
+        <ParentSize className={styles.bannerGallery}>
+          {props => <HomePageBannerGallery {...props} />}
+        </ParentSize>
+        <div className={styles.homepageBannerInner}>
+          <div className={styles.banner}>
+            <div
+              className={styles.bannerShadow}
+              style={{ textShadow: bannerTextShadow.join(', ') }}
+            >
+              {bannerText}
+            </div>
+            <div className={styles.bannerInner}>{bannerText}</div>
+          </div>
+          <div className={styles.bannerFeature}>
+            <Link to={'/articles'}>
+              <button className={styles.featureBtn}>
+                View Articles&nbsp; ›
+              </button>
+            </Link>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   )
 }
 
