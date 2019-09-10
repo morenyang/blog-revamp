c05ba6b4561f7a5e1576edbe10b42522ba29f194
Teng YANG <morenyang88@gmail.com>
2019-09-10 13:50:58 +0800

    Update homepage gallery
    - Update homepage banner height
    - Update homepage gallery

--- src/components/HomepageBannerGallery.js
+++ src/components/HomepageBannerGallery.js
@@ -1,15 +1,26 @@
-import React from 'react'
-
+import React, { useState } from 'react'
 import { Group } from '@vx/group'
 import { LinePath } from '@vx/shape'
 import { curveMonotoneX } from '@vx/curve'
 import { genDateValue } from '@vx/mock-data'
 import { scaleTime, scaleLinear } from '@vx/scale'
+import {
+  GradientDarkgreenGreen,
+  GradientLightgreenGreen,
+  GradientOrangeRed,
+  GradientPinkBlue,
+  GradientPinkRed,
+  GradientPurpleOrange,
+  GradientPurpleRed,
+  GradientPurpleTeal,
+  GradientSteelPurple,
+  GradientTealBlue,
+} from '@vx/gradient'
 import { extent, max } from 'd3-array'
 
 function genLines(num) {
   return new Array(num).fill(1).map(() => {
-    return genDateValue(25)
+    return genDateValue(20)
   })
 }
 
@@ -25,7 +36,7 @@ const y = d => d.value
 const HomePageBannerGallery = ({ width, height }) => {
   // bounds
   const xMax = width
-  const yMax = height / 8
+  const yMax = height / 6
 
   // scales
   const xScale = scaleTime({
@@ -37,8 +48,36 @@ const HomePageBannerGallery = ({ width, height }) => {
     domain: [0, max(data, y)],
   })
 
+  const gradientGroup = {
+    GradientDarkgreenGreen,
+    GradientLightgreenGreen,
+    GradientOrangeRed,
+    GradientPinkBlue,
+    GradientPinkRed,
+    GradientPurpleOrange,
+    GradientPurpleRed,
+    GradientPurpleTeal,
+    GradientSteelPurple,
+    GradientTealBlue,
+  }
+
+  const getRandomGradientName = () => {
+    const names = Object.keys(gradientGroup)
+    const index = Math.floor(Math.random() * names.length)
+    return names[index]
+  }
+
+  const [gradientName, setGradientName] = useState(getRandomGradientName())
+
+  const Gradient = gradientGroup[gradientName]
+
+  const handleClick = () => {
+    setGradientName(getRandomGradientName())
+  }
+
   return (
-    <svg width={width} height={height}>
+    <svg width={width} height={height} onClick={handleClick}>
+      <Gradient id="stroke" />
       <rect x={0} y={0} width={width} height={height} fill="rgba(0,0,0,0)" />
       {xMax > 8 &&
         series.map((d, i) => {
@@ -48,8 +87,8 @@ const HomePageBannerGallery = ({ width, height }) => {
                 data={d}
                 x={d => xScale(x(d))}
                 y={d => yScale(y(d))}
-                stroke={'#747474'}
-                strokeWidth={1}
+                stroke={'url(#stroke)'}
+                strokeWidth={1.5}
                 curve={i % 2 === 0 ? curveMonotoneX : undefined}
               />
             </Group>
