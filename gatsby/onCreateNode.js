a425a90ca8649d38131994fd04dc31dea3020dfd
Teng YANG <morenyang88@gmail.com>
2019-09-16 22:28:57 +0800

    Refactor: extra methods in onCreateNode

--- gatsby/onCreateNode.js
+++ gatsby/onCreateNode.js
@@ -1,61 +1,10 @@
-const { createFilePath } = require('gatsby-source-filesystem')
-const { getCategoryPathByPage } = require('../common/routerHelper')
 const draftControl = require('./nodes/draftControl')
-const _ = require('lodash')
+const createSlugField = require('./nodes/createSlugField')
+const createCoverField = require('./nodes/createCoverField')
+const createCollectionFieldByParentSourceInstanceName = require('./nodes/createCollectionField')
+const createCategoriesField = require('./nodes/createCategoriesField')
 
 const isNodeMarkdown = node => node.internal.type === 'MarkdownRemark'
-
-const createSlugField = ({ getNode, node, createNodeField }) => {
-  const slug = createFilePath({ node, getNode })
-  createNodeField({
-    node,
-    name: `slug`,
-    value: slug,
-  })
-}
-
-const createCoverField = ({ node, createNodeField }) => {
-  const coverImage = node.frontmatter.coverImage
-  createNodeField({
-    node: node,
-    name: `coverImage`,
-    value: coverImage,
-  })
-}
-
-const createCollectionFieldByParentSourceInstanceName = ({
-  getNode,
-  node,
-  createNodeField,
-}) => {
-  // Get the parent node
-  const parent = getNode(node.parent)
-
-  // Create a field on this node for the "collection" of the parent
-  // NOTE: This is necessary so we can filter `allMarkdownRemark` by
-  // `collection` otherwise there is no way to filter for only markdown
-  // documents of type `post`.
-  createNodeField({
-    node,
-    name: 'collection',
-    value: parent.sourceInstanceName,
-  })
-}
-
-const createCategoriesField = ({ node, createNodeField }) => {
-  const categories = node.frontmatter.categories || []
-
-  const categoriesFieldValue = categories.map(category => ({
-    category: _.startCase(category),
-    link: getCategoryPathByPage(category),
-  }))
-
-  createNodeField({
-    node,
-    name: `categories`,
-    value: categoriesFieldValue,
-  })
-}
 
 const onCreateNode = ({ node, actions, getNode }) => {
   const { createNodeField } = actions
