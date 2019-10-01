afae708b9af4df38854c23b87fee7ea3caed9585
Teng YANG <morenyang88@gmail.com>
2019-10-01 23:29:42 +0800

    Update colorful link

--- src/components/common/ColorfulLink.js
+++ src/components/common/ColorfulLink.js
@@ -21,21 +21,31 @@ const connectComponent = component =>
     mapDispatchToProps
   )(component)
 
+const getStyledLinkHover = props => `
+  text-decoration: ${props.isGradient ? props.gradient.from : '#0052cc'}
+    solid underline;
+  &:hover {
+    ${
+      props.isGradient
+        ? `
+    background-image: linear-gradient(to right, ${props.gradient.from}, ${props.gradient.to});
+    -webkit-text-fill-color: transparent;
+    background-clip: text;
+    -webkit-background-clip: text;`
+        : `color: '#0052cc'`
+    }
+  }
+`
+
+const WrappedColorfulAnchor = styled.a`
+  ${props => getStyledLinkHover(props)}
+`
+
+export const ColorfulAnchor = connectComponent(WrappedColorfulAnchor)
+
 const WrappedColorfulLinkWrapper = styled.div`
   a {
-    text-decoration: ${props =>
-        props.isGradient ? props.gradient.from : '#0052cc'}
-      solid underline;
-    &:hover {
-      ${props =>
-        props.isGradient
-          ? `
-      background-image: linear-gradient(to right, ${props.gradient.from}, ${props.gradient.to});
-      -webkit-text-fill-color: transparent;
-      background-clip: text;
-      -webkit-background-clip: text;`
-          : `color: '#0052cc'`}
-    }
+    ${props => getStyledLinkHover(props)}
   }
 `
 
