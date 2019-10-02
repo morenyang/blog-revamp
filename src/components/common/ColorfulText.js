7503872779f5aa8ae52988187084b40920f410be
Teng YANG <morenyang88@gmail.com>
2019-10-02 12:10:58 +0800

    Update gradient styles

---
+++ src/components/common/ColorfulText.js
@@ -0,0 +1,98 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import { randomGradientColor } from '../../state/actions/color'
+import styled from 'styled-components'
+import PropTypes from 'prop-types'
+
+const mapStateToProps = ({ color }) => ({
+  isGradient: color.isGradient,
+  gradient: color.gradient,
+})
+
+const mapDispatchToProps = dispatch => {
+  return {
+    randomGradientColor: bindActionCreators(randomGradientColor, dispatch),
+  }
+}
+
+const connectComponent = component =>
+  connect(
+    mapStateToProps,
+    mapDispatchToProps
+  )(component)
+
+const getTextDecoration = props =>
+  `text-decoration: ${
+    props.isGradient ? props.gradient.from : props.default || '#333'
+  } solid underline;`
+
+const getTextGradient = props =>
+  `${
+    props.isGradient
+      ? `
+    background-image: linear-gradient(45deg, ${props.gradient.from} 50%, ${props.gradient.to});
+    -webkit-text-fill-color: transparent;
+    background-clip: text;
+    -webkit-background-clip: text;`
+      : `color: ${props.default || '#333'}`
+  }
+  }`
+
+const getStyledLinkHover = props => `
+&:hover {
+  ${getTextGradient(props)}
+}
+`
+
+const WrappedColorfulAnchor = styled.a`
+  ${props => getTextDecoration(props)}
+  ${props => getStyledLinkHover(props)}
+`
+
+export const ColorfulAnchor = connectComponent(WrappedColorfulAnchor)
+ColorfulAnchor.defaultProps = {
+  default: '#0052cc',
+}
+
+const WrappedColorfulLinkWrapper = styled.div`
+  a {
+    ${props => getTextDecoration(props)}
+    ${props => getStyledLinkHover(props)}
+  }
+`
+
+export const ColorfulLinkWrapper = connectComponent(WrappedColorfulLinkWrapper)
+ColorfulLinkWrapper.defaultProps = {
+  default: '#0052cc',
+}
+
+const ColorfulTextInner = ({
+  children,
+  enableClick,
+  randomGradientColor: randomGradient,
+  className,
+}) => {
+  const handleTextClick = () => {
+    enableClick && randomGradient()
+  }
+  return (
+    <span className={className} onClick={handleTextClick}>
+      {children}
+    </span>
+  )
+}
+
+ColorfulTextInner.defaultProps = {
+  enableClick: false,
+}
+
+const WrappedColorfulText = styled(ColorfulTextInner)`
+  ${props => getTextGradient(props)}
+  user-select: none;
+`
+
+export const ColorfulText = connectComponent(WrappedColorfulText)
+ColorfulText.propTypes = {
+  enableClick: PropTypes.boolean,
+}
